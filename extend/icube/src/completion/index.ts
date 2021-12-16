afec544f99fa15edc1c83ccc9240364397b2c484
zhongconghai <zhongconghai@360.cn>
2021-12-16 19:49:25 +0800

    feat: extend

---
+++ extend/icube/src/completion/index.ts
@@ -0,0 +1,18 @@
+export default [
+    {
+        label: "getStorage",
+        insertText: "getStorage"
+    },
+    {
+        label: "getCubeInfo",
+        insertText: "getCubeInfo()" 
+    },
+    {
+        label: "getSystemInfo",
+        insertText: "getSystemInfo()" 
+    },
+    {
+        label: "clearStorage",
+        insertText: "clearStorage()" 
+    },
+];
--- extend/icube/src/extension.ts
+++ extend/icube/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import completion from './completion/index';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,6 +10,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "icube" is now active!');
+
+	vscode.languages.registerCompletionItemProvider('javascript', {
+		provideCompletionItems: (document, position) => {
+			return completion;
+	}}, '.');
+	
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
